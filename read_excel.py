53fdaa95af19e5d6b6853b476081b11ab62f5aeb
yuanborong <akb477@qq.com>
2020-05-28 11:10:35 +0800

    borong second commit.

--- read_excel.py
+++ read_excel.py
@@ -1,4 +1,5 @@
 from util import dataframe_from_csv
+from util import display
 import os
 import pandas as pd
 
@@ -11,10 +12,15 @@ def create_stays(table1_name , table2_name):
     stays = stays[['subject_id', 'gender', 'dob', 'dod' , 'intime']]
     return stays
 
+def process_stays_age(stays):
+    stays['age'] = round(((stays['intime'].subtract(stays['dob'])).apply(lambda x: x.days)) / 365)
+    stays.loc[stays.age < 0, 'age'] = 90
+    return stays
+
 # 合并patients和icustays表，计算age
 stays = create_stays('PATIENTS.csv' , 'ICUSTAYS.csv')
 stays['intime'] = pd.to_datetime(stays['intime'])
 stays['dob'] = pd.to_datetime(stays['dob'])
-stays['age'] = round(((stays['intime'].subtract(stays['dob'])).apply(lambda x : x.days)) / 365)
-stays.loc[stays.age < 0 , 'age'] = 90
-# print(stays)
+stays = process_stays_age(stays)
+
+display(stays)
--- util.py
+++ util.py
@@ -5,3 +5,10 @@ import pandas as pd
 
 def dataframe_from_csv(path, header=0, index_col=0):
     return pd.read_csv(path, header=header, index_col=index_col)
+
+def display(dataframe):
+    # 显示所有列
+    pd.set_option('display.max_columns', None)
+    # 显示所有行
+    pd.set_option('display.max_rows', None)
+    print(dataframe)
