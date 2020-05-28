b2a22cbbd216617d93dde9f84367a1a54aa9f44a
yuanborong <akb477@qq.com>
2020-05-28 12:02:53 +0800

    borong change "read_table" function.

--- read_excel.py
+++ read_excel.py
@@ -5,12 +5,37 @@ import pandas as pd
 
 mimic3_path = "mimic_data"
 
-def create_stays(table1_name , table2_name):
+# 读取patients表，返回pats[['subject_id' , 'gender' , 'dob' , 'dod']]
+def read_patients_table(mimic3_path):
+    pats = dataframe_from_csv(os.path.join(mimic3_path, 'PATIENTS.csv'))
+    pats = pats[['subject_id' , 'gender' , 'dob' , 'dod']]
+    pats['dob'] = pd.to_datetime(pats['dob'])
+    pats['dod'] = pd.to_datetime(pats['dod'])
+    return pats
 
-    table1 = dataframe_from_csv(os.path.join(mimic3_path, table1_name))
-    table2 = dataframe_from_csv(os.path.join(mimic3_path, table2_name))
-    stays = table1.merge(table2 , how='inner', left_on=['subject_id'], right_on=['subject_id'])
-    stays = stays[['subject_id', 'gender', 'dob', 'dod' , 'intime']]
+# 读取icustays表，
+def read_icustays_table(mimic3_path):
+    icustays = dataframe_from_csv(os.path.join(mimic3_path, 'ICUSTAYS.csv'))
+    icustays['intime'] = pd.to_datetime(icustays['intime'])
+    icustays['outtime'] = pd.to_datetime(icustays['outtime'])
+    return icustays
+
+# 读取admissions
+def read_admissions_table(mimic3_path):
+    admits = dataframe_from_csv(os.path.join(mimic3_path, 'ADMISSIONS.csv'))
+    admits = admits[['subject_id', 'hadm_id', 'admittime', 'dischtime', 'deathtime', 'ethnicity', 'diagnosis']]
+    admits['admittime'] = pd.to_datetime(admits['admittime'])
+    admits['dischtime'] = pd.to_datetime(admits['dischtime'])
+    admits['deathtime'] = pd.to_datetime(admits['deathtime'])
+    return admits
+
+# 合并patients,icustays和admissions表（整理ICU病人）
+def create_stays(mimi3_path):
+    pats = read_patients_table(mimic3_path)
+    icustays = read_icustays_table(mimic3_path)
+    admits = read_admissions_table(mimic3_path)
+    stays = (pats.merge(icustays , how='inner', left_on=['subject_id'], right_on=['subject_id'])).merge(admits , how='inner' , left_on=['subject_id'], right_on=['subject_id'])
+    stays = stays[['subject_id', 'gender',  'dob', 'dod' , 'intime' , ]]
     return stays
 
 # 合并patients和icustays表，计算age
@@ -19,10 +44,10 @@ def process_stays_age(stays):
     stays.loc[stays.age < 0, 'age'] = 90
     return stays
 
-
-stays = create_stays('PATIENTS.csv' , 'ICUSTAYS.csv')
+stays = create_stays(mimic3_path)
 stays['intime'] = pd.to_datetime(stays['intime'])
 stays['dob'] = pd.to_datetime(stays['dob'])
+stays['dod'] = pd.to_datetime(stays['dod'])
 stays = process_stays_age(stays)
 
-display(stays)
+display(stays.head())
